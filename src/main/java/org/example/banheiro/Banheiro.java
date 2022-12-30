d73c5ff9a40fe065d33d61f82f6a68d6d9c54364
Diogo de Souza Farias Pinto <dpinto@sdnadigital.com>
2022-12-30 15:05:44 -0400

    use of Lock class

--- src/main/java/org/example/banheiro/Banheiro.java
+++ src/main/java/org/example/banheiro/Banheiro.java
@@ -1,29 +1,37 @@
 package org.example.banheiro;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Banheiro {
 
+    Lock lock = new ReentrantLock();
+
     public void fazNumero1(){
+
         String nome = Thread.currentThread().getName();
 
         System.out.println(nome + " batendo na porta");
 
+
         /**
-         * Usa syncronize para executar o bloco sincronizadamente com possíveis outras threads. O bloco será executádo inteiramente
+         * Usa Lock para executar o bloco sincronizadamente com possíveis outras threads. O bloco será executádo inteiramente
          * antes de iniciar a execução de outra thread para o mesmo bloco. */
-        synchronized (this) {
-            System.out.println(nome + " entrando no banheiro");
-            System.out.println(nome + " fazendo coisa rápida");
 
-            try {
-                Thread.sleep(5000);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+        lock.lock();
+        System.out.println(nome + " entrando no banheiro");
+        System.out.println(nome + " fazendo coisa rápida");
 
-            System.out.println(nome + " dando descarga");
-            System.out.println(nome + " lavando a mão");
-            System.out.println(nome + " saindo do banheiro");
+        try {
+            Thread.sleep(5000);
+        }catch (Exception e){
+            e.printStackTrace();
         }
+
+        System.out.println(nome + " dando descarga");
+        System.out.println(nome + " lavando a mão");
+        System.out.println(nome + " saindo do banheiro");
+        lock.unlock();
     }
 
     public void fazNumero2(){
@@ -31,19 +39,19 @@ public class Banheiro {
 
         System.out.println(nome + " batendo na porta");
 
-        synchronized (this){
-            System.out.println(nome + " entrando no banheiro");
-            System.out.println(nome + " fazendo coisa demorada");
+        lock.lock();
+        System.out.println(nome + " entrando no banheiro");
+        System.out.println(nome + " fazendo coisa demorada");
 
-            try {
-                Thread.sleep(10000);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-
-            System.out.println(nome + " dando descarga");
-            System.out.println(nome + " lavando a mão");
-            System.out.println(nome + " saindo do banheiro");
+        try {
+            Thread.sleep(10000);
+        }catch (Exception e){
+            e.printStackTrace();
         }
+
+        System.out.println(nome + " dando descarga");
+        System.out.println(nome + " lavando a mão");
+        System.out.println(nome + " saindo do banheiro");
+        lock.unlock();
     }
 }
